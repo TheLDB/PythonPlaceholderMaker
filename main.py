ff3a061f438a3acaf029f8113deb96047936fe84
TheLDB <ldb@erikboles.com>
2020-11-24 08:03:54 -0700

    added and fixed

--- main.py
+++ main.py
@@ -9,21 +9,23 @@ blurred = input("Blurred? [y/n]: ")
 grayscale = input("Grayscale? [y/n]: ")
 export = input("Export data about image? [y/n]: ")
 if blurred == "y" and grayscale == "y":
-    r = requests.get("https://picsum.photos/" + sizeOne + "/" + sizeTwo + "/?grayscale&blur")
+    blurLevel = input("Enter the amount of blur (1-5): ")
+    r = requests.get("https://picsum.photos/" + sizeOne + "/" + sizeTwo + "/?grayscale&blur=" + blurLevel)
 elif blurred == "y":
-    r = requests.get("https://picsum.photos/" + sizeOne + "/" + sizeTwo + "/?blur")
+    blurLevel = input("Enter the amount of blur (1-5): ")
+    r = requests.get("https://picsum.photos/" + sizeOne + "/" + sizeTwo + "/?blur=" + blurLevel)
 elif grayscale == "y":
     r = requests.get("https://picsum.photos/" + sizeOne + "/" + sizeTwo + "/?grayscale")
 elif blurred != "y" and grayscale != "y":
     r = requests.get("https://picsum.photos/" + sizeOne + "/" + sizeTwo + "/")
 if export == "y":
     urlLink = str(r.url)
-    split = urlLink.split("/")[4]
+    split = urlLink.split("/")[4] # Splits the url from requests into different sections divided by /, the ID is the fifth element (4 starting from 0)
     newR = requests.get("https://picsum.photos/id/" + split + "/info")
+    urllib.request.urlretrieve(str(newR.url), fileName + ".txt")
 else: 
-    print("")
-# r = requests.get("https://picsum.photos/" + sizeOne + "/" + sizeTwo + "/?blur")
+    print("ok")
+# r = requests.get("https://picsum.photos/" + sizeOne + "/" + sizeTwo + "/?blur"
 urllib.request.urlretrieve(str(r.url), fileName + ".png")
-urllib.request.urlretrieve(str(newR.url), fileName + ".txt")
+print(str(r.url))
 print("Image Saved")
-print(split)
